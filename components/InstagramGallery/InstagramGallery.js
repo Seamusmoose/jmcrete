228ae9176c5f096f4fe0c04d613e3da5c3048562
Seamus O'Sullivan <info@seamuso.com>
2021-09-15 15:34:18 +0200

    insta gallery

--- components/InstagramGallery/InstagramGallery.js
+++ components/InstagramGallery/InstagramGallery.js
@@ -1,9 +1,14 @@
 import styles from './InstagramGallery.module.css'
 
-export default function InstagramGallery() {
+// import Instagram from 'instagram-web-api';
+
+export default function InstagramGallery({ instagramPosts }) {
     return (
         <div>
             <h1 className={styles.instagramGalleryC}>Instagram Gallery will go here</h1>
+            
         </div>
     )
 };
+
+
